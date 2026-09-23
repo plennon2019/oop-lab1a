076f929d098f2b15af4bef680727c13953e188ca
Paul Lennon <Paul.Lennon@atu.ie>
2026-09-23 16:44:45 +0100

    Added new method called displayDetails in Book

--- src/main/java/ie/atu/oop/week1/Book.java
+++ src/main/java/ie/atu/oop/week1/Book.java
@@ -8,6 +8,14 @@ public class Book {
 
     public Book()
     {
+    }
 
+    public void displayDetails()
+    {
+        System.out.println("This book is " + title);
+        System.out.println("The author of this book is " + author);
+        System.out.println("The page count of this book is " + pageCount);
+        System.out.println("The status of this book is " + available);
+        System.out.println("\n");
     }
 }
--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -12,21 +12,14 @@ public class Main {
         firstBook.author = "Frank Herbert";
         firstBook.pageCount = 412;
 
-        System.out.println("This book is " + firstBook.title);
-        System.out.println("The author of this book is " + firstBook.author);
-        System.out.println("The page count of this book is " + firstBook.pageCount);
-        System.out.println("The status of this book is " + firstBook.available);
-        System.out.println("\n");
+        firstBook.displayDetails();
 
         Book secondBook = new Book();
         secondBook.title = "The life and times of Thunderbolt";
         secondBook.author = "Frank Black";
         secondBook.pageCount = 12;
         secondBook.available = true;
-        System.out.println("This book is " + secondBook.title);
-        System.out.println("The author of this book is " + secondBook.author);
-        System.out.println("The page count of this book is " + secondBook.pageCount);
-        System.out.println("The status of this book is " + secondBook.available);
 
+        secondBook.displayDetails();
     }
 }
