c0099d250cb5795afcd6178091eaccf47cefa81d
Paul Lennon <Paul.Lennon@atu.ie>
2026-09-23 16:39:24 +0100

    Created second instance of Book class, and this object is called secondBook

--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -16,6 +16,17 @@ public class Main {
         System.out.println("The author of this book is " + firstBook.author);
         System.out.println("The page count of this book is " + firstBook.pageCount);
         System.out.println("The status of this book is " + firstBook.available);
+        System.out.println("\n");
+
+        Book secondBook = new Book();
+        secondBook.title = "The life and times of Thunderbolt";
+        secondBook.author = "Frank Black";
+        secondBook.pageCount = 12;
+        secondBook.available = true;
+        System.out.println("This book is " + secondBook.title);
+        System.out.println("The author of this book is " + secondBook.author);
+        System.out.println("The page count of this book is " + secondBook.pageCount);
+        System.out.println("The status of this book is " + secondBook.available);
 
     }
 }
