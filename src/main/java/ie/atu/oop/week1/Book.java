8c445dbf03d6625caa775eef1be83806376cfa30
Paul Lennon <Paul.Lennon@atu.ie>
2026-09-23 16:29:16 +0100

    Created new book class and created an instance in Main

---
+++ src/main/java/ie/atu/oop/week1/Book.java
@@ -0,0 +1,13 @@
+package ie.atu.oop.week1;
+
+public class Book {
+    public String title;
+    public String author;
+    public int pageCount;
+    public boolean available = true;
+
+    public Book()
+    {
+
+    }
+}
--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -7,5 +7,15 @@ public class Main {
     {
         System.out.println("Hello OOP");
 
+        Book firstBook = new Book();
+        firstBook.title = "Dune";
+        firstBook.author = "Frank Herbert";
+        firstBook.pageCount = 412;
+
+        System.out.println("This book is " + firstBook.title);
+        System.out.println("The author of this book is " + firstBook.author);
+        System.out.println("The page count of this book is " + firstBook.pageCount);
+        System.out.println("The status of this book is " + firstBook.available);
+
     }
 }
